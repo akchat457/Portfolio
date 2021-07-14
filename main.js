fab320da52771010b04881ec84f517731727fc3a
Akchat Gupta <69997542+akchat457@users.noreply.github.com>
2021-07-14 21:50:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 200;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 100;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+
+
+// Navbar
+
+
+const navSlide = () => {
+  let nav = document.querySelector('.nav-item');
+  let menuIcon = document.querySelector('.menu-icon');
+  let navLink = document.querySelectorAll('.links')
+
+  menuIcon.addEventListener('click', () => {
+    // toggle
+    nav.classList.toggle('nav-active')
+
+    // animate
+    navLink.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.3}s`
+      }
+    })
+    // burger animation
+    menuIcon.classList.toggle('toggle');
+  })
+
+  const listLinks = document.getElementsByClassName('nav-links');
+  for (var i = 0;i<listLinks.length;i++){
+   listLinks[i].addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+    menuIcon.classList.toggle('toggle');
+    navLink.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.3}s`
+      }
+    })
+
+  }) 
+  }
+  
+}
+navSlide();
